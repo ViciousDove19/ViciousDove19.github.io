4ce0e56fa4db39093504ea6eb531aea9a489d017
viciousdove19 <rautchinmay19@gmail.com>
2021-05-08 05:13:28 +0530

    ok

--- src/yourdata.js
+++ src/yourdata.js
@@ -5,6 +5,10 @@ import reactIcon from "./images/react.svg"
 import jsIcon from "./images/javascript.svg"
 import designIcon from "./images/design.svg"
 import codeIcon from "./images/code.svg"
+import pythonIcon from "./images/python.svg"
+import businessIcon from "./images/business.svg"
+import photoshopIcon from "./images/photoshop.svg"
+import LinkedInIcon from "./images/LinkedIn.svg"
 
 // Social Icon
 import githubIcon from "./images/github.svg"
@@ -12,92 +16,71 @@ import codepenIcon from "./images/codepen.svg"
 import dribbbleIcon from "./images/dribbble.svg"
 import instagramIcon from "./images/instagram.svg"
 
+
+
+//image files
+import MoM from './images/mom.png'
+import deer from './images/deer.png'
+import nightwonder from './images/nightwonder.jpg'
+import dystopia from './images/dystopia.jpg'
+
+
+
 export default {
   //(Please Do Not Remove The comma(,) after every variable)
   //Change The Website Template
 
   //   Header Details ---------------------
-  name: "chetan",
+  name: "Chinmay.",
   headerTagline: [
     //Line 1 For Header
-    "Building digital",
+    "Building products,",
     //Line 2 For Header
-    "products, brands,",
+    "Crafting experiences.",
     //Line 3 For Header
-    "and experience",
+    "Understanding ourselves.",
   ],
   //   Header Paragraph
   headerParagraph:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+    "I'm a third year student currently pursuing a dual degree in Engineering Design at IIT Madras.",
 
   //Contact Email
-  contactEmail: "hello@chetanverma.com",
+  contactEmail: "rautchinmay19@gmail.com",
 
   // End Header Details -----------------------
 
   // Work Section ------------------------
   projects: [
     {
-      title: "Project One", //Project Title - Add Your Project Title Here
+      title: "Maze of Malazzar - Pixel Art Game", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "A pixel game made with Godot Engine for a horror game jam.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        MoM,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://viciousdove19.itch.io/the-maze-of-malazzar",
     },
     {
-      title: "Project Two", //Project Title - Add Your Project Title Here
+      title: "Chase & Capture - Multiplayer Board Game", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "A multiplayer board game based on Scotland Yard and set in IIT Madras. Made with Godot Game Engine and Azure Virtual Machine for cloud computing.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+        deer,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://play.google.com/store/apps/details?id=org.godotengine.scotlandyard",
     },
     {
-      title: "Project Three", //Project Title - Add Your Project Title Here
+      title: "Energy Sector in India - Case Study", //Project Title - Add Your Project Title Here
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+        "Detailed case study on the energy sector of India and possible business opportunities.", // Add Your Service Type Here
       //Project Image - Add Your Project Image Here
       imageSrc:
-        "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        dystopia,
       //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
+      url: "https://dystopianfuturemovies.com/",
     },
-    {
-      title: "Project Four", //Project Title - Add Your Project Title Here
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
-    },
-    {
-      title: "Project Five", //Project Title - Add Your Project Title Here
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
-    },
-    {
-      title: "Project Six", //Project Title - Add Your Project Title Here
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc:
-        "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
-      //Project URL - Add Your Project Url Here
-      url: "http://chetanverma.com/",
-    },
-
     /*
     If You Want To Add More Project just Copy and Paste This At The End
     ,{
@@ -113,13 +96,13 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "I'm currently pursuing a dual degree in Engineering Design at IIT Madras. I love learning about new technologies and applying them to add value in society. Being passionate about entrepreneurship, I often find myself figuring out product-market fit for various ideas, fictional or non-fictional.",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "Outside of work, I enjoy watching anime, listening to podcasts, reading books and playing video games. I often try my hand at creating art to express my ideas, be it sketching, writing, painting or even game development. Some of my favourite creations include Attack on Titan, Monster, Mushishi, Silicon Valley, Breaking Bad, The Last of Us, God of War, Harry Potter, The Hobbit, Agatha Christie's mysteries, Murakami's magical realism, and Stephen King's horror novels. Clearly, I'm a pop-culture buff. I also love cycling and running, especially with a good song or podcast on!",
   aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "I believe experiences are much more valuable than materialistic pleasures. My idea of happiness is reading a cozy mystery with my dog by my side while it's raining outside.",
   aboutImage:
-    "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    nightwonder,
 
   //   End About Section ---------------------
 
@@ -129,34 +112,34 @@ export default {
 
   skills: [
     {
-      img: htmlIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
-      img: cssIcon,
-      para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    {
       img: jsIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "I've used JavaScript, HTML and CSS immensely to create meaningful web experiences. I'm skilled in libraries like React, Vue, and three.js.",
     },
     {
-      img: reactIcon,
+      img: pythonIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "Python is my go-to programming language. I'm skilled in Django for backend operations and integrating database systems in the framework. I'm familiar with ML and CV libraries like tensorflow, opencv and scikit-learn.",
+    },
+    {
+      img: photoshopIcon,
+      para:
+        "I've knowledge of Adobe's design tools like Photoshop, Illustrator, After Effects and XD. I'm also familiar with 3D softwares like Blender, Unity and Fusion 360.",
+    },
+    {
+      img: businessIcon,
+      para:
+        "I'm well versed with various project management tools and techniques and have applied them through my positions of responsibilty at IITM. I possess business development skills learnt through reading voraciously and interacting with leaders.",
     },
     {
       img: designIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
+        "I've been practicing data-driven User reasearch and UX design to find product-market fit. I'm aware of the cognitve science tools to understand and define customer needs.",
+    },    
     {
       img: codeIcon,
       para:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        "I have explored and implemented technologies like Cloud computing, Game development and Vitual reality. I'm currently learning more about Blockchain and Computer Vision and their role in Healthcare.",
     },
   ],
 
@@ -164,9 +147,9 @@ export default {
 
   //   Promotion Section --------------------------
 
-  promotionHeading: "Heading",
+  promotionHeading: "Connect With Me",
   promotionPara:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "If you have a project in mind where I could help or want to contribute to any of my projects, do reach out. I believe the world could be changed if the right people come together. Looking forward to an exciting conversation.",
   // End Promotion Section -----------------
 
   //   Contact Section --------------
@@ -174,18 +157,21 @@ export default {
   contactSubHeading: "Let's create your next experience together",
   social: [
     // Add Or Remove The Link Accordingly
-    { img: githubIcon, url: "https://github.com/chetanverma16" },
+    { 
+      img: githubIcon,
+      url: "https://github.com/ViciousDove19" 
+    },
     {
-      img: codepenIcon,
-      url: "https://www.codepen.com/",
+      img: LinkedInIcon,
+      url: "https://www.linkedin.com/in/chinmay-raut-09307a15a/",
     },
     {
       img: dribbbleIcon,
-      url: "https://dribbble.com/chetanverma",
+      url: "https://dribbble.com/viciousdove19",
     },
     {
       img: instagramIcon,
-      url: "https://www.instagram.com/",
+      url: "https://www.instagram.com/rautchinmay19/",
     },
   ],
 
